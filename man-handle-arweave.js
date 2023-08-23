8c8362db18a1b4b2bbf21297299fd364d1621567
C. L <82318965+leicester70@users.noreply.github.com>
2023-08-23 18:14:36 +0800

    play with sdk

--- man-handle-arweave.js
+++ man-handle-arweave.js
@@ -1,4 +1,9 @@
+import fetch from 'node-fetch';
 import Arweave from "arweave";
+
+global.Headers = fetch.Headers;
+global.fetch = fetch;
+
 const arweave = Arweave.init({
   host: "127.0.0.1", // localhost:1984
   port: 1984,
@@ -6,6 +11,56 @@ const arweave = Arweave.init({
   logging: true
 });
 
-// arweave.wallets.generate().then((key) => {
-//   console.log(JSON.stringify(key))
-// })
+// generate wallet Json Web Key
+let walletJWK;
+await arweave.wallets.generate().then((key) => { walletJWK = key })
+// console.log(walletJWK)
+
+// JWK to wallet Address; requires walletJWK;
+let walletAddress;
+await arweave.wallets.jwkToAddress(walletJWK).then((address) => { walletAddress = address })
+console.log(walletAddress)
+
+// get wallet balance; requires walletAddress;
+await arweave.wallets.getBalance(walletAddress).then((balance) => {
+  // winston reprsents the smallest unit in AR
+  let winston = balance;
+  console.log(winston)
+  let ar = arweave.ar.winstonToAr(balance);
+  console.log(ar)
+})
+
+// create transactions
+let tx_a_data = {
+  "payee": "ocbc",
+  "payer": "dbs",
+  "amount": 500000,
+  "currency": "SGD"
+}
+tx_a_data = JSON.stringify(tx_a_data)
+
+let transaction_A = await arweave.createTransaction({
+  data: tx_a_data
+}, walletJWK)
+
+console.log(transaction_A)
+
+let transaction_B = await arweave.createTransaction({
+  target: walletAddress,
+  quantity: arweave.ar.arToWinston(0.01),
+  "data": JSON.stringify({
+    "payee": "ocbc",
+    "payer": "maybank",
+    "amount": 10000,
+    "currency": "MYR"
+  })
+}, walletJWK)
+
+console.log(transaction_B)
+
+await arweave.transactions.sign(transaction_A, walletJWK)
+
+// get last transaction
+await arweave.wallets.getLastTransactionID(walletAddress).then((transactionId) => {
+  console.log(transactionId)
+})
